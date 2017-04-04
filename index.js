390b027f0ff8914fdb96e8ecfbac466e9b334edd
Nik Sudan <nik.sudan@digital-results.com>
2017-04-04 12:01:43 +0100

    Fallback for non Date objects as parameters

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const moment = require('moment');
+const isDate = require('is-date-object');
 
 module.exports = (fromDateRaw, toDateRaw = false) => {
-  const fromDate = moment(fromDateRaw);
-  if (toDateRaw === false) {
-    const toDate = moment();
-  } else {
-    const toDate = moment(toDateRaw);
-  }
+  const fromDate = moment((isDate(fromDateRaw)) ? fromDateRaw : new Date(fromDateRaw));
+  const toDate = (toDateRaw === false) ?
+    moment() :
+    (moment((isDate(toDateRaw)) ? toDateRaw : new Date(toDateRaw)));
   if (!fromDate || !toDate) {
     return false;
   }
